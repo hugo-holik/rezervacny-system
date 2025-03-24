ed1f434a63cc5b5d8e1219ade020744af40480e5
RadovanBulla1304 <radovan.bulla1304@gmail.com>
2025-03-24 16:12:50 +0100

    Added navigation items to the menu.
    User specific. User cant see the Panel Spravcu and externe skoly and cvicenia. Admin can see everything.

--- apps/frontend/src/pages/admin/index.jsx
+++ apps/frontend/src/pages/admin/index.jsx
@@ -1,9 +1,44 @@
+import Aplications from './Aplications';
+import Calendar from './Calendar';
+import Events from './Events';
+import Exercises from './Exercises';
+import ExternalSchools from './ExternalSchools';
+import History from './History';
+import Info from './Info';
 import UsersList from './UsersList';
 
 const routes = [
   {
     path: 'users',
     element: <UsersList />
+  },
+  {
+    path: 'external-schools',
+    element: <ExternalSchools />
+  },
+  {
+    path: 'exercises',
+    element: <Exercises />
+  },
+  {
+    path: 'events',
+    element: <Events />
+  },
+  {
+    path: 'calendar',
+    element: <Calendar />
+  },
+  {
+    path: 'applications',
+    element: <Aplications />
+  },
+  {
+    path: 'history',
+    element: <History />
+  },
+  {
+    path: 'info',
+    element: <Info />
   }
 ];
 
