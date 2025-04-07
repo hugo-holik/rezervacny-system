dd01efba8f96dbd860b83c83d92c9a80239090b0
ZulSomthin <61710101+ZulSomthin@users.noreply.github.com>
2025-04-07 09:49:45 +0200

    Pridanie kalendara

--- apps/frontend/src/pages/admin/Calendar.jsx
+++ apps/frontend/src/pages/admin/Calendar.jsx
@@ -1,5 +1,73 @@
+import React, { useEffect, useState } from 'react';
+import { useGetAllExercisesQuery } from '@app/redux/api';  
+import { Calendar as BigCalendar, momentLocalizer } from 'react-big-calendar';
+import moment from 'moment';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+
+
+const localizer = momentLocalizer(moment);
+
+const Calendar = () => {
+  const { data: exercises, isLoading, isError } = useGetAllExercisesQuery();
+  const [events, setEvents] = useState([]);  
+
+  // Mapovanie cviceni do kalendara
+  useEffect(() => {
+    // Kontrola ci su data dostupne
+    if (exercises) {
+      const mappedEvents = exercises.map(exercise => {
+        return exercise.startTimes.map(startTime => {
+          const startDate = moment(exercise._id).set({
+            hour: parseInt(startTime.split(':')[0]), 
+            minute: parseInt(startTime.split(':')[1]), 
+          });
+          const endDate = moment(startDate).add(exercise.duration, 'minutes');
+
+          return {
+            title: exercise.name,  
+            start: startDate.toDate(),  
+            end: endDate.toDate(),  
+            allDay: false,
+            color: exercise.color || '#00bcd4',
+          };
+        });
+      }).flat();  // Flatten the array in case there are multiple start times per exercise
+
+      setEvents(mappedEvents);
+    }
+  }, [exercises]);
+
+  if (isLoading) return <div>Načítavanie...</div>;
+  if (isError) return <div>Error lnačítavania</div>;
+
+  //Zobrazenie kalendara cez big-calendar
+  return (
+    <div style={{ height: '100vh' }}>
+      <BigCalendar
+        localizer={localizer}  
+        events={events}  
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: '100%' }} 
+        views={['month', 'week', 'day']}
+        
+        eventPropGetter={(event) => ({
+          style: {
+            backgroundColor: event.color,
+          }
+        })}
+      />
+    </div>
+  );
+};
+
+
+
+
+/*
 const Calendar = () => {
   return <div>Calendar</div>;
 };
+*/
 
 export default Calendar;
