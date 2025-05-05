4eb7aced2b713b820412211f99dfa9fe18c440fd
RadovanBulla1304 <radovan.bulla1304@gmail.com>
2025-05-05 23:00:10 +0200

    Delete aplication working

--- apps/frontend/src/Router.jsx
+++ apps/frontend/src/Router.jsx
@@ -2,13 +2,13 @@ import { createBrowserRouter } from 'react-router-dom';
 import MinimalLayout from './components/layout/MinimalLayout';
 import Protected from './components/layout/Protected';
 import { RouteNotFound } from './components/RouteNotFound.component';
+import EventDetail from './pages/admin/EventDetail';
 import Info from './pages/admin/Info';
 import authRoutes from './pages/auth';
 import Dashboard from './pages/dashboard/Dashboard';
-import EventDetail from './pages/admin/EventDetail';
 
 import adminRoutes from '@app/pages/admin';
-import { element } from 'prop-types';
+// import { element } from 'prop-types';
 
 export const router = createBrowserRouter([
   {
