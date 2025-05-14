0e56e55ec30ec659c97991e67e02b4dc8b43cbb0
Hugo Holik <hugo.holiq@gmail.com>
2025-05-14 21:29:36 +0200

    GetApplications refactor

--- apps/backend/src/controllers/eventController.js
+++ apps/backend/src/controllers/eventController.js
@@ -5,6 +5,7 @@ const Exercise = require("../models/exercise");
 const User = require("../models/user");
 const { validate, validated } = require("../util/validation");
 const { sendApplicationSchema } = require("../schemas/event.schema");
+const { buildApplicationsData } = require("../services/applicationsHelper");
 
 exports.get = async (req, res) => {
   try {
@@ -327,46 +328,20 @@ exports.sendApplication = [
   }
 ];
 
+//get curent user's applications
 exports.getApplications = async (req, res) => {
-  const currentUser = req.user;
   try {
     const events = await Event.find();
     const exercises = await Exercise.find();
-    const applications = [];
-
-    for (const event of events) {
-      for (const openExercise of event.openExercises) {
-        for (const attendee of openExercise.attendees) {
-          if (attendee.teacher.toString() === currentUser.user_id.toString()) {
-            const eventData = {
-              date: openExercise.date,
-              startTime: openExercise.startTime,
-              numOfAttendees: attendee.numOfAttendees,
-              approvalState: attendee.approvalStatus,
-              eventId: event._id,
-              dateClosing: event.dateClosing,
-              exerciseId: openExercise._id,
-              applicationId: attendee._id,
-              createdAt: attendee.createdAt,
-              approvedAt: attendee.approvedAt,
-            };
-
-            const foundExercise = exercises.find(
-              (e) => e._id.toString() === openExercise.exercise.toString()
-            );
-            if (foundExercise) {
-              eventData.maxAttendees = foundExercise.maxAttendees;
-              eventData.exerciseName = foundExercise.name;
-            }
-
-            applications.push(eventData);
-          }
-        }
-      }
-    }
+    const applications = await buildApplicationsData(events, exercises, {
+      teacherId: req.user.user_id
+    });
 
     res.send(applications);
-  } catch (error) { }
+
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
 };
 
 exports.editApplication = async (req, res) => {
---
+++ apps/backend/src/services/applicationsHelper.js
@@ -0,0 +1,40 @@
+exports.buildApplicationsData = async (events, exercises, filters = {}) => { 
+
+  const applications = [];
+
+  for (const event of events) {
+    for (const openExercise of event.openExercises) {
+      for (const attendee of openExercise.attendees) {
+
+        if (filters.teacherId && attendee.teacher.toString() !== filters.teacherId.toString()) {
+          continue;
+        }
+
+        const eventData = {
+          date: openExercise.date,
+          startTime: openExercise.startTime,
+          numOfAttendees: attendee.numOfAttendees,
+          approvalState: attendee.approvalStatus,
+          eventId: event._id,
+          dateClosing: event.dateClosing,
+          exerciseId: openExercise._id,
+          applicationId: attendee._id,
+          createdAt: attendee.createdAt,
+          approvedAt: attendee.approvedAt,
+        };
+
+        const foundExercise = exercises.find(
+          (e) => e._id.toString() === openExercise.exercise.toString()
+        );
+        if (foundExercise) {
+          eventData.maxAttendees = foundExercise.maxAttendees;
+          eventData.exerciseName = foundExercise.name;
+        }
+
+        applications.push(eventData);
+      }
+    }
+  }
+
+  return applications;
+};
